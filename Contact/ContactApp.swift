304f7b3fd5cd586c4b3d7aa208e54e39f69a75ae
red <wonseok@duck.com>
2023-02-24 15:37:19 +0900

    Initial Commit

---
+++ Contact/ContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactApp.swift
+//  Contact
+//
+//  Created by 레드 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
